3652e47583f63058b8e1b248200a2b3ad1a935d1
Ikenna Richard <oguejioforrichard@gmail.com>
2022-12-08 16:38:47 -1100

    Remove unecessary scripts

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+
 (function () {
     'use strict'
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = [...document.querySelectorAll('.needs-validation')]
 
-    const inputs = [...document.querySelectorAll('input')];
-  
-    // Loop over them and prevent submission
+
     forms
-      .forEach(function (form, i) {
+      .forEach(function (form) {
         form.addEventListener('submit', function (event) {
           if (!form.checkValidity()) {
             
